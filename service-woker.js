330e7282530392480da519b40e9e4e88e7dd8e2d
otto monteiro <afonsodsilvamendes@icloud.com>
2025-02-16 22:23:42 -0300

    will be temporary

---
+++ service-woker.js
@@ -0,0 +1,38 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open('my-cache').then((cache) => {
+        return cache.addAll([
+          '/',
+          '/login2.html',
+          '/Home.html',
+          '/cadastro.html',
+          './1.png',
+          './2.png',
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request).then((cachedResponse) => {
+        return cachedResponse || fetch(event.request);
+      })
+    );
+  });
+  
+  self.addEventListener('activate', (event) => {
+    const cacheWhitelist = ['my-cache'];
+    event.waitUntil(
+      caches.keys().then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cacheName) => {
+            if (!cacheWhitelist.includes(cacheName)) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    );
+  });
+  
